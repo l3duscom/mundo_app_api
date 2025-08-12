7fdb95560289c5048e285d46ab7017ade08e7760
Eduardo Santos <eduardosantos_777@icloud.com>
2025-08-11 21:48:38 -0300

    swagger

--- tests/integration/api/v1/users/post.test.js
+++ tests/integration/api/v1/users/post.test.js
@@ -129,11 +129,9 @@ describe("POST /api/v1/users", () => {
         operatorSession.token
       );
 
-      const responseBody = await response.json();
-      console.log('Operator response status:', response.status);
-      console.log('Operator response body:', responseBody);
-
       expect(response.status).toBe(403);
+
+      const responseBody = await response.json();
 
       expect(responseBody).toEqual({
         name: "ForbiddenError",
